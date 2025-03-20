20139265199234c21e86723e441ffcd802d6eb25
ramehnagell <rameshnagella272@gmail.com>
2025-03-20 16:48:17 +0530

    exploring get,post,put,patch,delete

---
+++ homework.md
@@ -0,0 +1,22 @@
+-Create Repository
+-Initialize Repository
+-node_modules, package.json, package_lock.json diff
+-Install express
+-Create server
+-LIsten to port
+-Write request Handlers for /test /hello
+-Install nodemon and update the scripts insdie package.json
+-What are dependencies
+-What is the use of "-g" flag while npm install
+-Difference between caret and tilde(^ vs ~)
+
+-initialize git
+-.gitignore file
+-Create a remote repo on github
+-Push all code to remote origin
+-Play with route and route extension ex. /hello, /, /hello/2, etc
+
+- /// Order of the routes matter
+
+-install Postman and make a workspcae/collection > make test API call
+-wirte logic to handle GET, POST, PUT, PATCH, DELET API calls and test themwith postman
--- src/app.js
+++ src/app.js
@@ -1,23 +1,63 @@
-const express = require("express");
+// const express = require("express");
 
-const app = express();
+// const app = express();
+/*
+// app.use("/", (req, res) => {
+//   res.send("hello from dashboard");
+// });
+
+app.use("/hello", (req, res) => {
+  res.send("hello hello");
+});
+app.use("/test", (req, res) => {
+  res.send("Hello from server!");
+});
+
+// app.use("/hello/2", (req, res) => {
+//   res.send("fsgk;lsdlkg");
+// });
 
 // app.use("/", (req, res) => {
 //   res.send("hello from dashboard");
 // });
 
-app.use("/test", (req, res) => {
-  res.send("Hello from server!");
-});
-
-app.use("/hello", (req, res) => {
-  res.send("hello hello");
-});
-
 app.use("/data", (req, res) => {
   res.send("data available here");
 });
+*/
+const express = require("express");
 
+const app = express();
+//This will only handle GET call to /user path
+app.get("/user", (req, res) => {
+  // res.send("Hello from server!");
+  res.send({ firstName: "Akshay", lastName: "Saini" });
+});
+
+//This will only handle POST call to /user path
+app.post("/user", (req, res) => {
+  //saving data to db
+  res.send("Data saved successfully.");
+});
+
+//This will only handle PUT api call to /user path
+app.put("/user", (req, res) => {
+  res.send("Data update successfully.");
+});
+//This will only handle PATCH API call to /user path
+app.patch("/user", (req, res) => {
+  res.send("Data modified successfully.");
+});
+
+//This will only handle DELETE call to /user path
+app.delete("/user", (req, res) => {
+  res.send("Data deleted.");
+});
+
+//This will match all th HTTP method API calls to /data path
+app.use("/data", (req, res) => {
+  res.send("data available here");
+});
 // app.listen(3000);
 app.listen(7777, () => {
   console.log("server connected succssfully and listening to the port.");
